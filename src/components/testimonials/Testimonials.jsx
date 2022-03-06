60a0fc0daa91cfb5efbfbe6287774d13ef470d75
su-tran75 <su-tran@hotmail.fr>
2022-03-07 01:12:04 +0800

    generating Testimonials from Array using map method

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -5,6 +5,33 @@ import avatar2 from "../../assets/avatar2.jpeg";
 import avatar3 from "../../assets/avatar3.jpeg";
 import avatar4 from "../../assets/avatar4.jpeg";
 
+const data = [
+  {
+    avatar: avatar1,
+    name: "Dallas Herman",
+    review:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque quasi quis, voluptas quae aut natus, fugiat officia recusandae nobis ad repellendus error omnis accusamus. Enim tempore perferendis suscipit doloremque eveniet itaque aspernatur ipsum dolorum adipisci, labore fugiat consequatur a voluptatum!",
+  },
+  {
+    avatar: avatar2,
+    name: "Claude Bartoletti",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto facilis necessitatibus quae reprehenderit optio voluptas animi velit corrupti, provident fugit ad tempore doloremque, nulla est quas veniam vitae culpa, odit possimus ratione amet rem voluptatem. Possimus rem deserunt eum nam.",
+  },
+  {
+    avatar: avatar3,
+    name: "Conner Heidenreich V",
+    review:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis fugiat officia doloremque laborum? Excepturi quidem, cumque quasi eligendi, necessitatibus ipsum natus et pariatur dolore, blanditiis veritatis animi mollitia? Quae, quisquam ad quidem iste minus accusantium nobis modi doloremque at dolores?",
+  },
+  {
+    avatar: avatar4,
+    name: "Karine Hegmann",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla autem voluptas saepe eius facere, corrupti impedit, ratione recusandae odit dignissimos est facilis blanditiis perspiciatis eaque similique doloribus ad asperiores beatae iure iste explicabo odio ullam quia possimus! Cum, dolore at!",
+  },
+];
+
 const Testimonials = () => {
   return (
     <section id="testimonials">
@@ -12,54 +39,17 @@ const Testimonials = () => {
       <h2>Testimonials</h2>
 
       <div className="container testimonials__container">
-        <article className="testimonial">
-          <div className="client__avatar">
-            <img src={avatar1} alt="Avatar One" />
-          </div>
-          <h5>Patricia Watsica</h5>
-          <small className="client__review">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos
-            inventore sit laudantium totam cumque praesentium ipsa explicabo eum
-            reiciendis exercitationem aspernatur id, ullam minima, incidunt
-            similique quasi deserunt voluptatem! Cum.
-          </small>
-        </article>
-        <article className="testimonial">
-          <div className="client__avatar">
-            <img src={avatar1} alt="Avatar One" />
-          </div>
-          <h5>Patricia Watsica</h5>
-          <small className="client__review">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos
-            inventore sit laudantium totam cumque praesentium ipsa explicabo eum
-            reiciendis exercitationem aspernatur id, ullam minima, incidunt
-            similique quasi deserunt voluptatem! Cum.
-          </small>
-        </article>
-        <article className="testimonial">
-          <div className="client__avatar">
-            <img src={avatar1} alt="Avatar One" />
-          </div>
-          <h5>Patricia Watsica</h5>
-          <small className="client__review">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos
-            inventore sit laudantium totam cumque praesentium ipsa explicabo eum
-            reiciendis exercitationem aspernatur id, ullam minima, incidunt
-            similique quasi deserunt voluptatem! Cum.
-          </small>
-        </article>
-        <article className="testimonial">
-          <div className="client__avatar">
-            <img src={avatar1} alt="Avatar One" />
-          </div>
-          <h5>Patricia Watsica</h5>
-          <small className="client__review">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos
-            inventore sit laudantium totam cumque praesentium ipsa explicabo eum
-            reiciendis exercitationem aspernatur id, ullam minima, incidunt
-            similique quasi deserunt voluptatem! Cum.
-          </small>
-        </article>
+        {data.map(({ avatar, name, review }, index) => {
+          return (
+            <article className="testimonial">
+              <div className="client__avatar">
+                <img src={avatar} alt="Avatar One" />
+              </div>
+              <h5>{name}</h5>
+              <small className="client__review">{review}</small>
+            </article>
+          );
+        })}
       </div>
     </section>
   );
